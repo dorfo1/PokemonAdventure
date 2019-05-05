68d173a5dfc86fd9aea53666631aa0020f6657b5
Rodolfo <rodolfo__BN@hotmail.com>
2019-05-05 18:02:32 -0300

    commit

---
+++ app/src/main/java/br/com/fiap/pokemonmobileadventure/adapter/UsuariosAdapter.kt
@@ -0,0 +1,39 @@
+package br.com.fiap.pokemonmobileadventure.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.fiap.model.User
+import br.com.fiap.pokemonmobileadventure.R
+
+class UsuariosAdapter(private var context: Context,
+                      private var usuarios:MutableList<User>,
+                      private var listener : (Unit) -> User) : RecyclerView.Adapter<UsuariosAdapter.UsuariosHolder>() {
+
+
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): UsuariosHolder {
+        var view = LayoutInflater.from(context).inflate(R.layout.row_treinador,p0,false)
+        return UsuariosHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+       return usuarios.size
+    }
+
+    override fun onBindViewHolder(holder: UsuariosHolder, position: Int) {
+        var usuario = usuarios.get(position)
+        holder.bindView(usuario,listener)
+    }
+
+
+    class UsuariosHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(usuario: Any, listener: (Unit) -> User) {
+            row_treinador_email.text = usuario.email
+            row_treinador_nome.text = usuario.nome
+        }
+    }
+}
---
+++ app/src/main/java/br/com/fiap/pokemonmobileadventure/ui/main/fragment/TraderFragment.kt
@@ -0,0 +1,46 @@
+package br.com.fiap.pokemonmobileadventure.ui.main.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.fiap.model.User
+import br.com.fiap.pokemonmobileadventure.R
+import com.google.firebase.database.*
+
+
+class TraderFragment : Fragment(){
+
+    private lateinit var mDatabase : FirebaseDatabase
+    private lateinit var ref : DatabaseReference
+    private val usuarios: MutableList<User> = mutableListOf()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        var view = inflater.inflate(R.layout.fragment_trader,container,false)
+
+        var usuarios : MutableList<User> = ArrayList()
+        mDatabase = FirebaseDatabase.getInstance()
+        ref = mDatabase.reference
+        initUsuarioList()
+
+        return view
+    }
+
+
+
+    private fun initUsuarioList() {
+        val menuListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                usuarios.clear()
+                dataSnapshot.children.mapNotNullTo(usuarios) { it.getValue<User>(User::class.java) }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+               //erro
+            }
+        }
+        ref.child("Usuario").addListenerForSingleValueEvent(menuListener)
+    }
+}
