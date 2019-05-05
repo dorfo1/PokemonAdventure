43a69591e2400956a3ad4ea54cda755eb0e76657
luiz <nunuzz13@gmasil.com>
2019-05-05 18:31:20 -0300

    algumas modificações

--- app/src/main/java/br/com/fiap/pokemonmobileadventure/model/Pokemon.kt
+++ app/src/main/java/br/com/fiap/pokemonmobileadventure/model/Pokemon.kt
@@ -1,7 +1,13 @@
 package br.com.fiap.model
 
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import com.google.gson.JsonObject
 
-data class Pokemon(  val id: Int,
-                     val name: String,
-                     val sprites: JsonObject)
+@Entity
+data class Time(
+    @ColumnInfo(name = "nome")var nome: String,
+    @ColumnInfo(name = "pokemon")var pokemon: JsonObject,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") var id: Long = 0
+)
