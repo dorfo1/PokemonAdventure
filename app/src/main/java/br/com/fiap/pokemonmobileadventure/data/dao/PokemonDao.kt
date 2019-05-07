9c3369a064a6b641386881a2779d03539cca88a6
Martin Sch <amrastasu@gmail.com>
2019-05-07 16:52:29 -0300

    - Alterada a classe Pokemon para não gravar o pokemon de novo se o registro do mesmo já existir. Assim não perdemos os dados de captura e etc.

--- app/src/main/java/br/com/fiap/pokemonmobileadventure/data/dao/PokemonDao.kt
+++ app/src/main/java/br/com/fiap/pokemonmobileadventure/data/dao/PokemonDao.kt
@@ -1,7 +1,9 @@
 package br.com.fiap.pokemonmobileadventure.data.dao
 
 import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Ignore
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy.IGNORE
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import android.arch.persistence.room.Query
 import br.com.fiap.model.Pokemon
@@ -12,7 +14,7 @@ interface PokemonDao {
     @Query("SELECT * from Pokemon")
     fun getAll(): List<Pokemon>
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = IGNORE )
     fun inserir(pokemon: Pokemon)
 
     @Query("UPDATE Pokemon SET nome = :nome, urlImagem = :img, capturado = :capturado WHERE id = :timeId")
