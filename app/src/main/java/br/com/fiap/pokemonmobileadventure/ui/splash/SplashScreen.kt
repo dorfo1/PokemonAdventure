f6ac5c38a2526ace9ce38eab815508d7c23ce86e
luiz <nunuzz13@gmasil.com>
2019-05-11 15:22:05 -0300

    animação splash + alguns icons

--- app/src/main/java/br/com/fiap/pokemonmobileadventure/ui/splash/SplashScreen.kt
+++ app/src/main/java/br/com/fiap/pokemonmobileadventure/ui/splash/SplashScreen.kt
@@ -6,10 +6,12 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.Window
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import br.com.fiap.pokemonmobileadventure.R
 import br.com.fiap.pokemonmobileadventure.ui.main.MainActivity
 import br.com.fiap.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class SplashScreen : AppCompatActivity() {
@@ -30,10 +32,22 @@ class SplashScreen : AppCompatActivity() {
         mAuth = FirebaseAuth.getInstance()
         if (mAuth.currentUser != null) {
             mudarTela(true)
+            load()
         }else{
             mudarTela(false)
+            load()
         }
     }
+
+    private fun load() {
+        val animation = AnimationUtils.loadAnimation(this, R.anim.splash)
+        animation.reset()
+        splash_background.startAnimation(animation)
+
+        Handler().postDelayed({
+        }, 3500L)
+    }
+
 
     private fun mudarTela(usuarioLogado : Boolean) {
         var runnable= object : Runnable {
